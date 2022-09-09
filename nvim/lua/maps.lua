3578b00dfdd3fb25dee752d7f5ba6d475446f827
JakWai01 <jakob.waibel@web.de>
2022-09-09 15:49:27 +0200

    feat: Modify behaviour when creating a new tab

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -14,7 +14,7 @@ keymap.set('n', 'ds', 'vb"_d')
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit', { silent = true })
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
