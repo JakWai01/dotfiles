b61939346e054adececb6e4a50eb09ff24acb7aa
Jakob-Waibel <jakob.waibel@ibm.com>
2023-11-13 12:18:10 +0100

    feat: Add nvim config

---
+++ nvim/README.md
@@ -0,0 +1,6 @@
+# Config
+
+## Files
+
+- `init.lua`: Contains Config
+- `lua-lock.json`: Contains the exact state of all your plugins
---
+++ nvim/init.lua
@@ -0,0 +1,163 @@
+-- Lazy Package Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+-- Options
+vim.g.mapleader = ' '
+
+vim.opt.rtp:prepend(lazypath)
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+-- Plugins
+require("lazy").setup({
+	'rmehri01/onenord.nvim',
+	"neovim/nvim-lspconfig",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-vsnip",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/vim-vsnip",
+	"simrat39/rust-tools.nvim",
+	"itchyny/lightline.vim",
+    "nvim-telescope/telescope.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+})
+
+-- Theme
+require('onenord').setup()
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Cmp
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+local cmp = require("cmp")
+
+cmp.setup({
+  preselect = cmp.PreselectMode.None,
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+  },
+
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+})
+
+-- LSP
+local function on_attach(client, buffer)
+	local keymap_opts = { buffer = buffer }
+	vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+	vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
+	vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+	vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
+	vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+end
+
+-- LaTeX
+require'lspconfig'.texlab.setup{}
+
+-- Go
+require'lspconfig'.gopls.setup{}
+
+-- Python
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
+
+-- Rust
+require("rust-tools").setup({
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  server = {
+    on_attach = on_attach,
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+})
